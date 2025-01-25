5082764586cbc4f09188f2c091256f9f6d4eb402
Md. Shakil <shakil.eub.cse@gmail.com>
2025-01-25 11:17:07 +0600

    Update code

--- djangojwt/myapp/serializers.py
+++ djangojwt/myapp/serializers.py
@@ -15,11 +15,11 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     last_name = serializers.CharField()
     email = serializers.EmailField()
     password = serializers.CharField(write_only=True)
-    password2 = serializers.CharField(write_only=True)
+    confirm_password = serializers.CharField(write_only=True)
     
     class Meta:
         model = User
-        fields = ['id','username','first_name','last_name','email','password','password2']
+        fields = ['id','username','first_name','last_name','email','password','confirm_password']
         
     def validate_username(self, username):
         if User.objects.filter(username=username).exists():
@@ -29,7 +29,7 @@ class UserRegisterSerializer(serializers.ModelSerializer):
             raise ValidationError(detail=detail)
         return username
     def validate(self, instance):
-        if instance['password'] != instance['password2']:
+        if instance['password'] != instance['confirm_password']:
             raise ValidationError({"message":"Both password must match"})
         if User.objects.filter(email=instance['email']).exists():
             raise ValidationError({"message":"Email already taken!"})
@@ -37,7 +37,7 @@ class UserRegisterSerializer(serializers.ModelSerializer):
     
     def create(self, validated_data):
         password = validated_data.pop('password')
-        password2 = validated_data.pop('password2')
+        confirm_password = validated_data.pop('confirm_password')
         user = User.objects.create(**validated_data)
         user.set_password(password)
         user.save()
