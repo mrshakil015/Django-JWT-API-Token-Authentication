e85de09624812e4680cef895aed734bc8bde9c88
Md. Shakil <shakil.eub.cse@gmail.com>
2025-01-25 10:36:32 +0600

    Api Documentation Include

--- djangojwt/myapp/urls.py
+++ djangojwt/myapp/urls.py
@@ -2,12 +2,26 @@ from django.urls import path,include
 from .views import UserRegisterViewSet,LoginView, DashboardView
 from rest_framework.routers import DefaultRouter
 
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API documentation for Django JWT API Token Authentication",
+        default_version="v1",
+        description="API documentation for Django JWT API Token Authentication",
+    ),
+    public=True
+)
+
+
 router = DefaultRouter()
 router.register('users', UserRegisterViewSet, basename='user')
 
 urlpatterns = [
     path('',include(router.urls)),
     path('login/',LoginView.as_view(), name='login'),
-    path('dashboard/',DashboardView.as_view(), name='dashboard'),     
+    path('dashboard/',DashboardView.as_view(), name='dashboard'),  
+    path('docs/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='redoc'),   
     
 ]
